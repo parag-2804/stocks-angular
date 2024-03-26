41f6cf5aff43debbbadec79c6230518f735a9ab4
Parag Jadhav <parag2804@gmail.com>
2024-03-25 23:48:14 -0700

    Added Project

---
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { StatePreserveService } from '../state-preserve.service';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+  public data: string = '';
+  // is
+  constructor(private statePreseve: StatePreserveService) { }
+
+  ngOnInit(): void {
+
+    this.statePreseve.tickerSymbolSubject.subscribe(res => {
+      this.data = res;
+      console.log('state>> ',res);
+    }
+    )
+  }
+}
