5c4f8be6439b849c549268c76c26ce9f93722ca3
Parag Jadhav <parag2804@gmail.com>
2024-03-28 13:24:12 -0700

    Footer Fixed

--- frontend/src/app/footer/footer.component.spec.ts
+++ frontend/src/app/footer/footer.component.spec.ts
@@ -1,16 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FooterComponent } from './footer.component';
 
-describe('FooterComponent', () => {
+describe('FooterComponent Tests', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     await TestBed.configureTestingModule({
       declarations: [ FooterComponent ]
-    })
-    .compileComponents();
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +17,7 @@ describe('FooterComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('ensures component instantiation', () => {
+    expect(component).toBeDefined();
   });
 });
